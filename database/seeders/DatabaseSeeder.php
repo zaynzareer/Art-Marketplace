0e4ffc99fba7eb335a21f6452ae42eea23194c0d
zaynzareer <zaynzareer@gmail.com>
2025-12-28 23:23:29 +0530

    Add: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cart;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\CartItem;
+use App\Models\OrderItem;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +20,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // $user = User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // // User::factory()->create([
+        // //     'name' => 'Test User',
+        // //     'email' => 'test@example.com',
+        // // ]);
+
+        // Product::factory(10)->for($user)->create();
+
+        User::factory(10)->create();
+
+        Product::factory(20)->create();
+
+        Cart::factory(8)->create();
+
+        Order::factory(15)->create();
+
+        OrderItem::factory(40)->create();
+
+        CartItem::factory(30)->create();
     }
 }
